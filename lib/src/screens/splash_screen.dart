51daf8518886c4940c336e1c2154af09064a164c
Ahmed <atarek017@gmail.com>
2020-09-09 11:45:02 +0200

    add splash screen

---
+++ lib/src/screens/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ms_app_round3/src/screens/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _spashScreenState();
+  }
+}
+
+class _spashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                HomePage()
+            )
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  radius: 1.5,
+                  colors: [
+                    Colors.lightGreenAccent,
+                    Colors.green,
+                  ],
+                ),
+              ),
+              child: Center(
+                child: Image.asset("assets/design.PNG"),
+              ),
+            ),
+            Positioned(
+                top: 70,
+                left: 20,
+                right: 20,
+                child: Text(
+                  "WELCOME TO MAZ APP",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
